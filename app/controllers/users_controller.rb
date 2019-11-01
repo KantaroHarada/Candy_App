0574e0440eeca85f2d05ada3ad28b09244b6ff24
Kantaro Harada <harada.kantaroh@lmi.ne.jp>
2019-11-01 09:11:12 +0000

    remember me token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ end
 private
 
     def user_params
-      params.require(:user).permit(:name, :email, :adress, :password,
+      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
 
@@ -46,7 +46,6 @@ private
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
-        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
@@ -55,6 +54,6 @@ private
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
